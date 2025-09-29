b2a824a23dc8e109e8725e04469b5fabf4581abe
Kaming Lo <lokaming86@gmail.com>
2025-09-29 13:05:48 +0700

    Adding some details

--- public/js/kaming/fetcher.js
+++ public/js/kaming/fetcher.js
@@ -14,13 +14,14 @@ function renderCards(data) {
         const section = data[key];
         const card = document.createElement("div");
         card.classList.add("card");
+        card.id = "card";
         card.innerHTML = `
             <img src="https://img.youtube.com/vi/${section.videos[0].link}/hqdefault.jpg" alt="Foto Default ${key}">
             <h3>${key}</h3>
             <div>
                 <p>${section.short_description}</>
             </div>
-            <a>Beli Kursus</a>
+            <a href="./checkout.html?key=${key}">Beli Kursus</a>
         `;
 
         container.appendChild(card);
